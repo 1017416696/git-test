7e5be257c54e8c4ad8129a437cec5aed7ec1db3c
Penrose <1017416696@qq.com>
2022-02-15 23:19:27 +0800

    master commit

--- src/main/java/com/penrose/git/GitTest.java
+++ src/main/java/com/penrose/git/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is a test");
         System.out.println("This is a test");
-        System.out.println("This is a hot-fix test");
+        System.out.println("This is a master test");
     }
 }
