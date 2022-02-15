16adf9c9815f6a35c6cf7764146c5d8ec0b837a9
Penrose <1017416696@qq.com>
2022-02-15 22:59:38 +0800

    second commit

--- src/main/java/com/penrose/git/GitTest.java
+++ src/main/java/com/penrose/git/GitTest.java
@@ -3,5 +3,6 @@ package com.penrose.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("This is a test");
+        System.out.println("This is a test");
     }
 }
